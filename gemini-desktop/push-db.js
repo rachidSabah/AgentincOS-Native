7f71e7713800579ebd8272a276e8671bfc2fee0f
Z User <z@container>
2026-06-07 18:56:44 +0000

    chore: remove gemini-desktop from git (contains secret in app.db)

--- gemini-desktop/push-db.js
+++
@@ -1,6 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  execSync('npx prisma@6.11.1 db push --schema=prisma/schema.prisma', { stdio: 'inherit' });
-} catch (e) {
-  console.error(e);
-}
